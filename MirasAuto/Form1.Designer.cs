a63c7d99a277364622bb10e9c8ae38d200fbfc5d
IraqSQ <ir4q@o2.pl>
2016-05-24 15:33:28 +0200

    1

---
+++ MirasAuto/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MirasAuto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loading = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 149);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loading
+            // 
+            this.loading.AutoSize = true;
+            this.loading.Location = new System.Drawing.Point(12, 149);
+            this.loading.Name = "loading";
+            this.loading.Size = new System.Drawing.Size(30, 13);
+            this.loading.TabIndex = 1;
+            this.loading.Text = "tekst";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 179);
+            this.Controls.Add(this.loading);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "MirasAuto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label loading;
+    }
+}
+
---
+++ MirasAuto/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace MirasAuto
+{
+    public partial class Form1 : Form
+    {
+        string[] config = { "english" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            CONFIG_Init();
+            LANG_Init();
+            loading.Text = LANG(config[0], 1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void CONFIG_Init()
+        {
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            if (File.Exists(path + "/config.txt") == false)
+            {
+                FileStream file = new FileStream(path + "/config.txt", FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            }
+            else
+            {
+                string line = "english";
+                int line_count = File.ReadLines(path + "/config.txt").Count();
+                System.IO.StreamReader file = new System.IO.StreamReader(path + "/config.txt");
+                for (int i = 0; i < line_count; i++)
+                {
+                    line = file.ReadLine();
+                    if (line != null)
+                    {
+                        var parameter = line.Split('=');
+                        config[i] = parameter[1];
+                    }
+                }
+            }
+        }
+
+        public void LANG_Init()
+        {
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            if (!Directory.Exists(path + "/lang")) Directory.CreateDirectory(path + "/lang");
+        }
+
+        public string LANG(string lg, int id)
+        {
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            if ((Directory.Exists(path + "/lang") == true) && (File.Exists(path + "/lang/" + lg + ".txt")))
+            {
+                string line = "???";
+                System.IO.StreamReader file = new System.IO.StreamReader(path + "/lang/" + lg + ".txt");
+                for (int i = 0; i < id; i++) line = file.ReadLine();
+
+                if (line != null) return line;
+            }
+            return "???";
+        }
+    }
+}
